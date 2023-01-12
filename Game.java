e330af5120e71e23a6155dd5821a42ba3d09ed30
Jason Wang <91506829+Leek727@users.noreply.github.com>
2023-01-12 13:34:41 +0000

    GAme class

--- Game.java
+++ Game.java
@@ -1,3 +1,45 @@
+import java.util.Random;
+
 public class Game {
+    public int numplayers;
+    public boolean solved;
+    public int round;
+    public int pointvalue;
+
+    public void play(int numplayers){
+        Player[] players = new Player[numplayers];
+        for (int i = 0; i < numplayers; i++){
+            players[i] = new Player();
+            players[i].name = "Test";
+        }
+
+        
+
+        
+    }
     
+    private void incrementRound(){
+
+    }
+
+    private void editScore(){
+
+    }
+
+    private void getInput(){
+
+    }
+
+    private void displayPts(){
+
+    }
+
+    private void victory(){
+
+    }
+    
+    private changePtValue(){
+        Random rand = new Random(); 
+        this.pointvalue = rand.nextInt(10_000_000);
+    }
 }
