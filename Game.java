917c056c649c4cd3a3f2028dbb5aa72a8892bcab
Grayson Welch <gwelch1002778@gmail.com>
2023-01-13 00:58:44 +0000

    changes

--- Game.java
+++ Game.java
@@ -1,4 +1,5 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class Game {
     public int numplayers;
@@ -6,39 +7,48 @@ public class Game {
     public int round;
     public int pointvalue;
 
-    public void play(int numplayers){
+    Scanner input = new Scanner(System.in);
+
+    Player[] players;
+
+    public void play(int numplayers) {
+
+        this.solved = false;
+
         Player[] players = new Player[numplayers];
         for (int i = 0; i < numplayers; i++){
             players[i] = new Player();
             players[i].name = "Test";
         }
-
-        
-
-        
     }
     
-    private void incrementRound(){
-
+    private void incrementRound() {
+        this.round += 1;
     }
 
-    private void editScore(){
-
+    private void editScore() {
+        //Don't know what this is for
     }
 
-    private void getInput(){
-
+    private String getInput(String what) {
+        System.out.println(what + "\n >:");
+        String data = input.next();
+        return data;
     }
 
-    private void displayPts(){
-
+    private void displayPts() {
+        System.out.println("------------SCOREBOARD------------");
+        for (Player player : this.players) {
+            System.out.println(player.name + ": " + player.score);
+        }
+        System.out.println("------------------------");
     }
 
-    private void victory(){
-
+    private void victory(String victoriousPlayer) {
+        System.out.println("The game has been won by " + victoriousPlayer);
     }
     
-    private changePtValue(){
+    private void changePtValue() {
         Random rand = new Random(); 
         this.pointvalue = rand.nextInt(10_000_000);
     }
--- Phrase.java
+++ Phrase.java
@@ -2,14 +2,24 @@ class Phrase {
     private String phraseString;
     private char[] guessedCharacters;
     public int phraseLength;
+
     
     public Phrase() {
+        //initalize game elements
         this.phraseString = createPhrase();
         this.phraseLength = phraseString.replaceAll("\\s", "").length();
         this.guessedCharacters = new char[phraseLength];
+
+        //initalize file reading elements
+        //still working on
     }
 
     private String createPhrase() {
         return "Nothing yet";
     }
+
+    /*
+    private String grabWord() {
+        return "Nothing Yet";
+    } */
 }
